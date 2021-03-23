49a6e18e255c0c619115bc1957ea7d2e1e246fe1
Caceresenzo <caceresenzo1502@gmail.com>
2021-03-23 21:54:32 +0100

    feat(user): Added 42 OAuth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:marvin]
 
   validates :username, uniqueness: true, presence: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token
+      user.username = auth.info.nickname
+    end
+  end
 
   def gravatar_url
     gravatar_id = Digest::MD5::hexdigest(email).downcase
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_scope :user do
+    delete "sign_out", :to => "devise/sessions#destroy", :as => :destroy_user_session_path
+  end
 
   root controller: :chatrooms, action: :index
 
