6021bb10ee3c440496a4d31514e113b33f86f7f0
Caceresenzo <caceresenzo1502@gmail.com>
2021-04-22 16:56:12 +0200

    feat(front): Using methods instead of names

--- app/javascript/packs/router.js
+++ app/javascript/packs/router.js
@@ -6,97 +6,95 @@ const User = require("./components/user");
 
 const Router = Backbone.Router.extend({
   routes: {
-    "": "home",
-    channels: "channels",
-    "channel/create": "channelCreate",
-    "channel/:id": "channelById",
-    "channel/:id/edit": "channelByIdEdit",
-    guilds: "guilds",
-	profile: "profile",
-	"profile/edit": "profileEdit",
-	"users": "users",
-	"user/:id": "userById"
+    ""() {
+      this.setActive(null);
+
+      this.app.setView(new Dummy.EmptyView());
+    },
+    channels() {
+      this.setActive("channels");
+
+      var channelCollection = new Channel.ChannelCollection();
+
+      this.app.setView(
+        new Channel.ChannelListView({
+          collection: channelCollection,
+        })
+      );
+
+      channelCollection.fetch();
+    },
+    "channel/create"() {
+      this.setActive("channels");
+
+      this.app.setView(
+        new Channel.ChannelCreateOrEditView({
+          id: null,
+        })
+      );
+    },
+    "channel/:id"(id) {
+      this.setActive("channels");
+
+      this.app.setView(
+        new Channel.ChannelView({
+          id,
+        })
+      );
+    },
+    "channel/:id/edit"(id) {
+      this.setActive("channels");
+
+      this.app.setView(
+        new Channel.ChannelCreateOrEditView({
+          id,
+        })
+      );
+    },
+    guilds() {
+      this.setActive("guilds");
+
+      this.app.setView(new Dummy.EmptyView());
+    },
+    profile() {
+      this.setActive("profile");
+
+      this.app.setView(
+        new User.UserProfileView({
+          user_id: current_user.id,
+        })
+      );
+    },
+    "profile/edit"() {
+      this.setActive("profile");
+
+      this.app.setView(new User.UserProfileEditView());
+    },
+    users() {
+      this.setActive("users");
+
+      var userCollection = new User.UserCollection();
+
+      this.app.setView(
+        new User.UserListView({
+          collection: userCollection,
+        })
+      );
+
+      userCollection.fetch();
+    },
+    "user/:id"(id) {
+      this.setActive("users");
+
+      this.app.setView(
+        new User.UserProfileView({
+          user_id: id,
+        })
+      );
+    },
   },
   initialize(options) {
     this.app = options.app;
-  },
-  home() {
-    this.setActive(null);
-
-    this.app.setView(new Dummy.EmptyView());
-  },
-  channels() {
-    this.setActive("channels");
-
-	console.log(Channel)
-
-    var channelCollection = new Channel.ChannelCollection();
-
-    this.app.setView(
-      new Channel.ChannelListView({
-        collection: channelCollection,
-      })
-    );
-
-    channelCollection.fetch();
-  },
-  channelCreate() {
-    this.setActive("channels");
-
-    this.app.setView(new Channel.ChannelCreateOrEditView());
-  },
-  channelById(id) {
-    this.setActive("channels");
-
-    this.app.setView(
-      new Channel.ChannelView({
-        id
-      })
-    );
-  },
-  channelByIdEdit(id) {
-    this.setActive("channels");
-
-    this.app.setView(
-      new Channel.ChannelCreateOrEditView({
-        id
-      })
-    );
-  },
-  guilds() {
-    this.setActive("guilds");
-
-    this.app.setView(new Dummy.EmptyView());
-  },
-  profile() {
-    this.setActive("profile");
-
-    this.app.setView(new User.UserProfileView({
-		user_id: current_user.id
-	}));
-  },
-  profileEdit() {
-    this.setActive("profile");
-	
-    this.app.setView(new User.UserProfileEditView());
-  },
-  users() {
-    this.setActive("users");
-
-    var userCollection = new User.UserCollection();
-
-    this.app.setView(new User.UserListView({
-		collection: userCollection
-	}));
-
-    userCollection.fetch();
-  },
-  userById(user_id) {
-    this.setActive("users");
-
-    this.app.setView(new User.UserProfileView({
-		user_id
-	}));
   },
   setActive(route) {
     this.app.navigationBarView.setActive(route);
